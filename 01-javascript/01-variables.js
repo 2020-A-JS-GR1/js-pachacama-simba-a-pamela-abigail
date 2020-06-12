a0fb7aca286edc9460155639d0afab09e611d9e3
Pamela Pachacama <pamela.pachacama@epn.edu.ec>
2020-06-12 17:20:19 -0500

    tipos de variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,35 @@
+//tipos de variables
+//mutables e inmutables
+//
+//mutables
+var numeroUno = 1;
+let numeroDos =  2;
+
+numeroUno = 5;
+numeroDos = 8;
+
+numeroUno = false;
+numeroDos = true;
+
+// inmutables
+const configuracionArchivo ="PDF";
+//configuracionArchivo = "XML";
+
+// tipos de variables
+
+const numero =1; //number
+const sueldo = 1.2; //number
+const texto = "Pamela"; // string
+const booleanoo = false; // boolean
+const hijos = null; // object
+const zapatos = undefined; // undefined
+
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleanoo);
+console.log(typeof hijos);
+console.log(typeof zapatos);
+
+
