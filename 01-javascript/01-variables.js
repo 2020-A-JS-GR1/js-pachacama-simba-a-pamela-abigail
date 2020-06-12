ffbd5e82d8d0cd6fba667552c535972f2041e449
Pamela Pachacama <pamela.pachacama@epn.edu.ec>
2020-06-12 18:00:07 -0500

    crear objetos js

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -23,7 +23,7 @@ const texto = "Pamela"; // string
 const booleanoo = false; // boolean
 const hijos = null; // object
 const zapatos = undefined; // undefined
-const apellido = 'Pachacama'; //
+//const apellido = 'Pachacama'; //
 
 console.log(typeof numero);
 console.log(typeof sueldo)
@@ -31,7 +31,7 @@ console.log(typeof texto)
 console.log(typeof booleanoo);
 console.log(typeof hijos);
 console.log(typeof zapatos);
-console.log(typeof apellido)
+//console.log(typeof apellido)
 console.log(Number("asd"));
 
 if(true == true){
@@ -61,11 +61,11 @@ if("Pamela"){
 if(-1){
     console.log("es truty");
 } else{
-    console.log("es falsy")
+    console.log("es falso") //truty
 }
 
 if(0){
-    console.log("es truty");
+    console.log("es truty"); //falsy
 } else{
     console.log("es falsy")
 }
@@ -73,8 +73,49 @@ if(0){
 if(1){
     console.log("es truty");
 } else{
-    console.log("es falsy")
+    console.log("es falsy") //truty
 }
+
+if(null){
+    console.log("es truty");
+} else{
+    console.log("es falso") //falsy
+}
+
+if(undefined){
+    console.log("es truty");
+} else{
+    console.log("es falso") //falsy
+}
+
+//orden de importancia
+//1) const
+//2) let
+//3) X-> "var" nunca
+
+// objetos Js (JSON) - Arreglos
+
+const pamela = {
+    nombre: "Pamela", //lleva: valor,
+    "apellido":'Pachacama',
+    edad: 24,
+    hijos: null,
+    zapatos:undefined,
+    casado: false,
+    ropa:{
+        color: 'rojo',
+        talla:40
+    },
+    mascotas:['cachetes','pequitas','panda'],
+}; // object
+pamela.nombre;//"Pamela"
+pamela.apellido;//'Pachacama
+
+const arregloNumeros = []; //object
+
+console.log(pamela);
+console.log(arregloNumeros);
+
 
 
 
