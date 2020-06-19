8d9728d03835c2a92c46ebb4b6fa14a4070e0ad6
Pamela Pachacama <pamela.pachacama@epn.edu.ec>
2020-06-19 16:55:35 -0500

    arreglos y comienzo de operadores

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -1,4 +1,4 @@
-const arreglo = [1,2,3,4,5,6,7,8,9,10];
+const arreglo = [2,1,4,3,5,6,7,8,9,10];
 //arreglo =1;
 /*let cualquierCosa="ASD";
 cualquierCosa = 1;
@@ -11,3 +11,33 @@ console.log(cualquierCosa);*/
 const arregloTodo=[
     true,1,1.2,undefined,null,{},[1,2,true,"A"]
 ];
+
+let a=[1,2,3];
+const b = Object.assign([],a);
+
+// for of
+
+for (let numero of arreglo){ //valores
+    console.log('numero',numero);
+}
+// for in
+for (let numero in arreglo){ //indice
+    console.log('numero',numero);
+}
+
+// const arreglo = [6,7,8,9,10];
+arreglo.push(11);
+//console.log(arreglo)
+//const arreglo = [6,7,8,9,10,11];
+arreglo.pop();
+//console.log(arreglo)
+//const arreglo = [5,6,7,8,9,10,11];
+arreglo.unshift(5); //Anadir al principio del arreglo
+//console.log(arreglo)
+arreglo.splice(0,1);
+//console.log(arreglo)
+arreglo.splice(0,0,3,4,5);
+const indice = arreglo.indexOf(9);
+console.log('indice',indice);
+arreglo.splice(indice,1);
+console.log(arreglo);
