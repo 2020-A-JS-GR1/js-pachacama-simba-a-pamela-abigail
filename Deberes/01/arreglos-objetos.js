7d399aa70de7c5d7e9f6a360bcd49c3e4a35a248
Pamela Pachacama <pamela.pachacama@epn.edu.ec>
2020-06-23 16:17:52 -0500

    deber

---
+++ Deberes/01/arreglos-objetos.js
@@ -0,0 +1,82 @@
+const arreglo = [
+    {
+        id:1,
+        nombre: 'Adrian',
+        nota: 5
+    },
+    {
+        id:2,
+        nombre: 'Vicente',
+        nota: 8
+    },
+    {
+        id:3,
+        nombre: 'Carolina',
+        nota: 14
+    },
+    {
+        id:4,
+        nombre: 'Wendy',
+        nota: 16
+    },
+    {
+        id:5,
+        nombre: 'Andrea',
+        nota: 19
+    },
+    {
+        id:6,
+        nombre: 'Pamela',
+        nota: 19
+    },
+    {
+        id:7,
+        nombre: 'Cristian',
+        nota: 20
+    },
+    {
+        id:8,
+        nombre: 'Daniel',
+        nota: 19
+    },
+    {
+        id:9,
+        nombre: 'Lilly',
+        nota: 14
+    },
+    {
+        id:10,
+        nombre: 'Ramiro',
+        nota: 12
+    }
+];
+
+let vocales=['a','e','i','o','u','A','E','I','O','U'];
+
+let sum=0;
+const respuestaMap = arreglo
+    .map(
+        (valorActual,indiceActual,arregloCompleto)=> {
+            const nombreaux = valorActual.nombre;
+
+            for(vocales in nombreaux){
+                sum= valorActual.nota+ 1;
+            }
+            const nuevoElemento = {
+                id: valorActual.id,
+                nombre:valorActual.nombre,
+                nota: sum,
+            };
+            return nuevoElemento;
+        }
+    );
+console.log('respuestaMap', respuestaMap); //undefined
+
+const respuestaFilter = arreglo
+    .filter(
+        (valorActual,indiceActual,arregloCompleto)=>{
+            return valorActual.nota >= 14;
+        }
+    )
+console.log('respuestaFilter', respuestaFilter); //undefined
+console.log('arreglo',arreglo)
