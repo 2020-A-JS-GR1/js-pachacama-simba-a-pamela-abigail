3df77cdab497d75e8bb6b1f477ec31f721c3a843
Pamela <pamela.pachacama@epn.edu.ec>
2020-09-04 17:30:44 -0500

    Componentes y directivas

--- 03-angular/mi-proyecto/src/app/app.component.ts
+++ 03-angular/mi-proyecto/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-proyecto';
+
+  mensajeConsola(objeto:boolean){
+    console.log('Llego el evento', objeto);
+  }
+
+  arregloPeliculas=[
+    {
+      id:1,
+      url:'https://es.web.img3.acsta.net/pictures/18/03/16/15/33/3988420.jpg',
+      descripcion:'pelicula accion',
+      nombrePelicula:'Advenger'
+    },
+    {
+      id:2,
+      url:'https://i.blogs.es/fa749e/aladdin-2019/450_1000.jpeg',
+      descripcion:'pelicula infantil',
+      nombrePelicula:'Aladin'
+    },
+    {
+      id:3,
+      url:'https://d500.epimg.net/cincodias/imagenes/2018/06/29/smarttv/1530279996_778826_1530280220_noticia_normal.jpg',
+      descripcion:'pelicula accion',
+      nombrePelicula:'Jumanji'
+    }
+]
+  arregloNumeros=[1,2,3]
 }
--- 03-angular/mi-proyecto/src/app/componentes/carta-pelicula/carta-pelicula.component.ts
+++ 03-angular/mi-proyecto/src/app/componentes/carta-pelicula/carta-pelicula.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-carta-pelicula',
@@ -16,6 +16,12 @@ export class CartaPeliculaComponent implements OnInit {
   @Input()
   nombreBoton: string;
 
+  @Output()
+  eventoDioclicBoton:EventEmitter<boolean> = new EventEmitter<boolean>()
+
+  @Output()
+  eventoDioclicImagen:EventEmitter<boolean> = new EventEmitter<boolean>()
+
   textoEjemplo = 'Pamela';
   urlEjemploImagen='https://es.web.img3.acsta.net/pictures/18/03/16/15/33/3988420.jpg';
   linkTextoEjemplo='https://www.google.com.ec/';
@@ -30,5 +36,10 @@ export class CartaPeliculaComponent implements OnInit {
   eventoOnBlur(){
     console.log('Blur')
   }
-
+  ejecutarEventoDioClic(){
+    this.eventoDioclicBoton.emit(true)
+  }
+  ejecutarEventoDioClicImagen(){
+    this.eventoDioclicImagen.emit(true)
+  }
 }
